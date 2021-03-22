d46940bcd69c6ec8c0080d108ea9eba7f6859565
areenjaradat <areenjaradat96@gmail.com>
2021-03-22 18:38:29 +0200

    solve all features

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Lab-02
+Number and name of feature: Feature #1: Display images
+
+Estimate of time needed to complete: 1hour
+
+Start time: 2:40pm
+
+Finish time: 3:13pm
+
+Actual time needed to complete: 30minutes
+///////////////////////////////////////////////////
+
+Number and name of feature: Feature #2: Filter images
+
+Estimate of time needed to complete: 1hour
+
+Start time: 2:14pm
+
+Finish time: 5:51pm
+
+Actual time needed to complete: 2 hours
+
+////////////////////////////////////////////
+
+Number and name of feature: Feature #3: Style the application
+
+Estimate of time needed to complete: 30minutes
+
+Start time: 5:51pm
+
+Finish time: 5:20pm
+
+Actual time needed to complete: 2 hours
---
+++ js/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+let arrayOfObjects=[];
+
+function Data(image_url,title,description,keyword,horns){
+    this.image_url=image_url;
+    this.title=title;
+    this.description=description;
+    this.keyword=keyword;
+    this.horns=horns;
+    arrayOfObjects.push(this);
+}
+Data.prototype.render=function(){
+    // console.log("create render");
+    let imgaeDisplay=$('#photo-template').clone();
+    // console.log(imgaeDisplay)
+    $('main').append(imgaeDisplay);
+    imgaeDisplay.find('h2').text(this.title);
+    imgaeDisplay.find('img').attr('src',this.image_url);
+    imgaeDisplay.find('p').text(this.description);
+    imgaeDisplay.removeAttr('id');
+
+    imgaeDisplay.attr('class','divClass');
+    
+};
+
+function getData(){
+    
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
+// console.log("start");
+    $.ajax('data/page-1.json',ajaxSettings).then(data=>{
+        // console.log("inside the ajax");
+        data.forEach(element => {
+            // console.log("create object");
+            // console.log(element.image_url,,element.description,element.keyword,element.horns);
+            let getdata=new Data(element.image_url,element.title,element.description,element.keyword,element.horns);
+            getdata.render();
+            // console.log(getdata)  
+            $('select').append($('<option>', {
+                // value: element.keyword,
+                text: element.keyword
+            }));
+
+        });
+    })
+
+}
+// $(document).ready(function() {
+
+//     //   $('#photo-template').toggleClass('on');
+//     // $('#photo-template').remove();
+   
+//     // $('select').change(function(){
+// $('select').on('click',function(){
+// //  $('#photo-template').empty();
+//     // $(this).siblings('#photo-template').toggleClass('on');
+   
+//    let theOption= $("select option:selected" ).text();
+
+//     console.log($( "select option:selected" ).text());
+
+//     arrayOfObjects.forEach((element,index)=>{
+//     if (arrayOfObjects[index].keyword===theOption){
+//         arrayOfObjects[index].render();
+//     }
+// });
+// })
+// });
+
+$(document).ready(function() {
+    
+$('select').change(function() {
+    $('.divClass').remove();
+   let theOption= $("select option:selected" ).text();
+
+    console.log($( "select option:selected" ).text());
+   
+    arrayOfObjects.forEach((element,index)=>{
+    if (arrayOfObjects[index].keyword===theOption){
+        arrayOfObjects[index].render();
+    }
+})
+arrayOfObjects.forEach((element,index)=>{
+    if ('Filter by Keyword'===theOption){
+        arrayOfObjects[index].render();
+    }
+})
+})//.trigger('change');
+
+})
+
+
+console.log(arrayOfObjects);
+$('document').ready(getData);
+// $('document').ready(filllist);
+// $( window ).load(filllist);
